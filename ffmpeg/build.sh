bdea1b10ceddaa84b100d29669a9f5ab76ce0336
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2017-05-28 01:34:17 +0900

    Update build.sh

--- ffmpeg/build.sh
+++ ffmpeg/build.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 cd $(dirname $0)
-set -ex
+set -e
 
 container_name=ffmpeg_copy
 image_name=ymyzk/ffmpeg
 
+if [ -z ${NUM_PROC+x} ]; then
+  echo '$NUM_PROC is unset, using default value: 2'
+  NUM_PROC=2
+fi
+
+set -x
 docker build \
     --build-arg CFLAGS="$CFLAGS" \
     --build-arg CPPFLAGS="$CPPFLAGS" \
